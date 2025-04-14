653f16bf02ba48ea48b926ea34df084201c47a0f
SirBroccoli <carlospolop@gmail.com>
2025-04-15 00:04:20 +0200

    Update translator.py

--- scripts/translator.py
+++ scripts/translator.py
@@ -190,6 +190,8 @@ Also don't add any extra stuff in your response that is not part of the translat
         return translate_text(language, text, file_path, model, cont, False, client)
 
     response_message = response.choices[0].message.content.strip()
+    response_message = response_message.replace("bypassy", "bypasses") # PL translations translates that from time to time
+    response_message = response_message.replace("Bypassy", "Bypasses")
 
     # Sometimes chatgpt modified the number of "#" at the beginning of the text, so we need to fix that. This is specially important for the first line of the MD that mucst have only 1 "#"
     cont2 = 0
